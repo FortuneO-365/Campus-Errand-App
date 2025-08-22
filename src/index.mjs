5bded6c1a223fb9a936a2b37a4b494518ff883e5
Fortune Obioha <fortuneobioha926@gmail.com>
2025-08-22 10:08:34 +0100

    cors fix

--- src/index.mjs
+++ src/index.mjs
@@ -25,7 +25,7 @@ const userSocketMap = new Map();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // Adjust this to your frontend's origin
+    origin: ['http://localhost:3000', 'http://localhost:5173', ], // Adjust this to your frontend's
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
